4b5820e945e548db80bbbeed816c3426b065d2d8
Can Toğran <106720789+cantogr07@users.noreply.github.com>
2024-01-23 14:22:18 +0300

    change arm method

--- src/main/java/frc/robot/commands/arm/ArmClosedLoop.java
+++ src/main/java/frc/robot/commands/arm/ArmClosedLoop.java
@@ -35,7 +35,7 @@ public class ArmClosedLoop extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    mArm.setArmAngleMotionMagic(setpoint, cruiseVel, acceleration);
+    mArm.setArmPosition(setpoint);
   }
 
   // Called once the command ends or is interrupted.
