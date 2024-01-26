a6346a742764dedb06914f268b37f1c20654fd1b
Can Toğran <106720789+cantogr07@users.noreply.github.com>
2024-01-26 12:12:49 +0300

    new shouldFlipAlliance method

--- src/main/java/frc/robot/commands/auto/ARCTrajectory.java
+++ src/main/java/frc/robot/commands/auto/ARCTrajectory.java
@@ -2,6 +2,8 @@ package frc.robot.commands.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
@@ -17,7 +19,16 @@ public class ARCTrajectory {
 
     private void configurePathPlanner() {
         AutoBuilder.configureHolonomic(swerve::getPoseMeters, swerve::resetOdometry, swerve::getChassisSpeed,
-                swerve::setClosedLoopStates, Constants.holonomicPoseConfig, swerve::shouldAlianceFlippedToRed, swerve);
+                swerve::setClosedLoopStates, Constants.holonomicPoseConfig, swerve::shouldAllianceFlipToRed, swerve);
+        /* 
+                AutoBuilder.configureHolonomic(swerve::getPoseMeters, swerve::resetOdometry, swerve::getChassisSpeed,
+                swerve::setClosedLoopStates, Constants.holonomicPoseConfig, () -> {
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                }, swerve); */
     }
 
     public Command getAutoPath(String pathName) {
