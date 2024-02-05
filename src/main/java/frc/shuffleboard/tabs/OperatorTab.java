a0be77efb42747b118f084c2f8c54987ac5f98fa
Can Toğran <106720789+cantogr07@users.noreply.github.com>
2024-02-05 22:24:32 +0300

    arm shuffleboard tab layout

--- src/main/java/frc/shuffleboard/tabs/OperatorTab.java
+++ src/main/java/frc/shuffleboard/tabs/OperatorTab.java
@@ -1,17 +1,20 @@
 package frc.shuffleboard.tabs;
 
+import com.ctre.phoenix6.signals.MotionMagicIsRunningValue;
+
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.CANdleLed;
 import frc.shuffleboard.ShuffleboardTabBase;
+import frc.team6014.lib.math.Conversions;
 
 public class OperatorTab extends ShuffleboardTabBase{
 
     /* ARM SUBSYSTEM */
     private ArmSubsystem mArm = ArmSubsystem.getInstance();
-    private GenericEntry boreAngle, falconAngle, armState, atSetpoint, atZero;
+    private GenericEntry boreAngle, falconAngle, armState, atSetpoint, atZero, motMagStatus, lastRotation, current, voltage;
 
     /* LEDS */
     //private CANdleLed mLed = CANdleLed.getInstance();
@@ -28,13 +31,13 @@ public class OperatorTab extends ShuffleboardTabBase{
         mTab = Shuffleboard.getTab("Operator");
 
         boreAngle = mTab
-        .add("Bore Angle", 0)
+        .add("A-Bore Angle", 0)
         .withPosition(0, 0)
         .withSize(1, 1)
         .getEntry();
 
         falconAngle = mTab
-        .add("Falcon Angle", 0)
+        .add("A-Falcon Angle", 0)
         .withPosition(0, 1)
         .withSize(1, 1)
         .getEntry();
@@ -46,7 +49,7 @@ public class OperatorTab extends ShuffleboardTabBase{
         .getEntry();
 
         atSetpoint = mTab
-        .add("At Setpoint", false)
+        .add("A-At Setpoint", false)
         .withPosition(1, 0)
         .withWidget(BuiltInWidgets.kBooleanBox)
         .withSize(1, 1)
@@ -59,6 +62,31 @@ public class OperatorTab extends ShuffleboardTabBase{
         .withWidget(BuiltInWidgets.kBooleanBox)
         .getEntry();
 
+        motMagStatus = mTab
+        .add("A-MMagic Working", false)
+        .withPosition(1, 2)
+        .withSize(1,1)
+        .withWidget(BuiltInWidgets.kBooleanBox)
+        .getEntry();
+
+        lastRotation = mTab
+        .add("A-Last Dem. Rotation", 0)
+        .withPosition(2, 0)
+        .withSize(1,1)
+        .getEntry();
+
+        current = mTab
+        .add("A-Current", 0)
+        .withPosition(2, 1)
+        .withSize(1,1)
+        .getEntry();
+
+        voltage = mTab
+        .add("A-Voltage", 0)
+        .withPosition(2, 2)
+        .withSize(1,1)
+        .getEntry();
+
         // currentAnimation = mTab
         // .add("Current Anim", "NONE")
         // .withPosition(2, 0)
@@ -69,11 +97,16 @@ public class OperatorTab extends ShuffleboardTabBase{
     @Override
     public void update() {
         /* ARM */
-        boreAngle.setDouble(truncate(mArm.getArmAngleBore()));
-        falconAngle.setDouble(truncate(mArm.getArmAngleFalcon()));
+        boreAngle.setDouble(truncate(Conversions.revolutionsToDegrees(mArm.getArmAngleBore())));
+        falconAngle.setDouble(truncate(Conversions.revolutionsToDegrees(mArm.getArmAngleFalcon())));
         armState.setString(mArm.getArmControlState().toString());
         atSetpoint.setBoolean(mArm.isAtSetpointFalcon());
         atZero.setBoolean(mArm.isAtZeroFalcon());
+        motMagStatus.setBoolean(mArm.getArmMotor().getMotionMagicIsRunning().getValue() == MotionMagicIsRunningValue.Enabled);
+        lastRotation.setDouble(Conversions.revolutionsToDegrees(mArm.getLastDemandedRotation()));
+        current.setDouble(mArm.getArmMotor().getStatorCurrent().getValueAsDouble());
+        voltage.setDouble(mArm.getArmMotor().getMotorVoltage().getValueAsDouble());
+
 
         /* LEDS */
         //currentAnimation.setString(mLed.getCurrentAnimation().toString());
