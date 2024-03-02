e0b84325fd6d68e9f4c3d844196856d6c00f5a01
MrPand-21 <alperacar751@gmail.com>
2024-03-02 20:56:13 +0300

    last

--- src/main/java/frc/robot/commands/RotateToSpeaker.java
+++ src/main/java/frc/robot/commands/RotateToSpeaker.java
@@ -2,13 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.limelight;
+package frc.robot.commands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Unit;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,26 +14,22 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.team6014.lib.decorators.Assign;
-import frc.team6014.lib.decorators.Assign.Prog;
 
 public class RotateToSpeaker extends Command {
   private final DriveSubsystem mDrive = DriveSubsystem.getInstance();
 
-  private final ProfiledPIDController m_thetaController = new ProfiledPIDController(7,
+  private final ProfiledPIDController m_thetaController = new ProfiledPIDController(DriveConstants.anglekP,
       DriveConstants.anglekI,
       DriveConstants.anglekD, DriveConstants.rotPIDconstraints);
 
-  @Assign(users = { Prog.Carabelli, Prog.CAN, Prog.Xerem }, message = "Angle FeedForward Ekleyek bier ara")
-  private final SimpleMotorFeedforward m_Feedforward = new SimpleMotorFeedforward(1, 0.1, 0);
   private double tethaSpeed = 0;
 
-  // in Degrees
   private double targetAngle;
 
   /** Creates a new RotateToSpeaker. */
   public RotateToSpeaker() {
-    m_thetaController.setTolerance(3);
+    m_thetaController.setTolerance(Math.toRadians(3));
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(mDrive);
   }
 
@@ -53,34 +47,24 @@ public class RotateToSpeaker extends Command {
     tethaSpeed = m_thetaController.calculate(mDrive.getPose().getRotation().getRadians(),
         Units.degreesToRadians(targetAngle));
 
-    mDrive.swerveDrive(0, 0, tethaSpeed, true);
+    mDrive.swerveDrive(0, 0, Units.radiansToDegrees(tethaSpeed), true);
     SmartDashboard.putNumber("LL Target Angle", targetAngle);
     SmartDashboard.putBoolean("LL Rotate SP", m_thetaController.atGoal());
-    SmartDashboard.putNumber("LL T S", (tethaSpeed));
+    SmartDashboard.putNumber("LL T S", Units.radiansToDegrees(tethaSpeed));
   }
 
   public double calcTargetAngle() {
-    double tAngle = 0;
-    double tetha = 0;
-    double RobotX = mDrive.getPose().getX();
-    double RobotY = mDrive.getPose().getY();
+    double tAngle = mDrive.getPose().getRotation().getDegrees();
     if (DriverStation.getAlliance().get() == Alliance.Blue) {
-      tetha = Units.radiansToDegrees(Math.acos(RobotX
-          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.BLUE_SPEAKER.getTranslation())));
-      tAngle = 180 - tetha;
+      tAngle = 180 - Math.acos(mDrive.getPose().getX()
+          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.BLUE_SPEAKER.getTranslation()));
     } else {
-      tetha = Units.radiansToDegrees(
-          Math.acos((Constants.FieldConstants.FieldX - RobotX)
-              / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.RED_SPEAKER.getTranslation())));
-      tAngle = -tetha;
+      tAngle = Math.acos((Constants.FieldConstants.FieldX - mDrive.getPose().getX())
+          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.RED_SPEAKER.getTranslation()));
     }
+    if (mDrive.getPose().getY() < Constants.FieldConstants.BLUE_SPEAKER.getY())
+      tAngle = tAngle * -1;
 
-    if (RobotY < Constants.FieldConstants.BLUE_SPEAKER.getY())
-      tAngle = -tAngle;
-
-    if (tAngle < 0) {
-      tAngle += 360;
-    }
     return tAngle;
   }
 
