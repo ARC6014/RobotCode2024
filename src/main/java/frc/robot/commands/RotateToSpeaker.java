e79e3b033cc66f0a19573cf16214657de6266834
sanker07 <92315405+sanker07@users.noreply.github.com>
2024-03-02 16:22:33 +0300

    LL Rotate To Speaker Final
    I Hope So, -BENİM DÜN YAPTIĞIMDAN BAŞKA BİR ŞEY YAPMADIK SIKINTI O - XEREM

--- src/main/java/frc/robot/commands/RotateToSpeaker.java
+++ src/main/java/frc/robot/commands/RotateToSpeaker.java
@@ -5,8 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,6 +16,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.team6014.lib.decorators.Assign;
+import frc.team6014.lib.decorators.Assign.Prog;
 
 public class RotateToSpeaker extends Command {
   private final DriveSubsystem mDrive = DriveSubsystem.getInstance();
@@ -21,14 +25,18 @@ public class RotateToSpeaker extends Command {
   private final ProfiledPIDController m_thetaController = new ProfiledPIDController(DriveConstants.anglekP,
       DriveConstants.anglekI,
       DriveConstants.anglekD, DriveConstants.rotPIDconstraints);
+
+  @Assign(users = { Prog.Carabelli, Prog.CAN, Prog.Xerem }, message = "Angle FeedForward Ekleyek bier ara")
+  // private final SimpleMotorFeedforward m_Feedforward = new
+  // SimpleMotorFeedforward(DriveConstants., 0, 0)
   private double tethaSpeed = 0;
 
+  // in Degrees
   private double targetAngle;
 
   /** Creates a new RotateToSpeaker. */
   public RotateToSpeaker() {
-    m_thetaController.setTolerance(Math.toRadians(3));
-    // Use addRequirements() here to declare subsystem dependencies.
+    m_thetaController.setTolerance(3);
     addRequirements(mDrive);
   }
 
@@ -43,26 +51,33 @@ public class RotateToSpeaker extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    tethaSpeed = m_thetaController.calculate(mDrive.getPose().getRotation().getRadians(),
-        Units.degreesToRadians(targetAngle));
+    tethaSpeed = m_thetaController.calculate(mDrive.getPose().getRotation().getDegrees(),
+        targetAngle);
 
-    mDrive.swerveDrive(0, 0, Units.radiansToDegrees(tethaSpeed), true);
+    mDrive.swerveDrive(0, 0, Units.degreesToRadians(tethaSpeed), true);
     SmartDashboard.putNumber("LL Target Angle", targetAngle);
     SmartDashboard.putBoolean("LL Rotate SP", m_thetaController.atGoal());
     SmartDashboard.putNumber("LL T S", Units.radiansToDegrees(tethaSpeed));
   }
 
   public double calcTargetAngle() {
-    double tAngle = mDrive.getPose().getRotation().getDegrees();
+    double tAngle = 0;
+    double tetha = 0;
+    double RobotX = mDrive.getPose().getX();
+    double RobotY = mDrive.getPose().getY();
     if (DriverStation.getAlliance().get() == Alliance.Blue) {
-      tAngle = 180 - Math.acos(mDrive.getPose().getX()
-          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.BLUE_SPEAKER.getTranslation()));
+      tetha = Units.radiansToDegrees(Math.acos(RobotX
+          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.BLUE_SPEAKER.getTranslation())));
+      tAngle = 180 - tetha;
     } else {
-      tAngle = Math.acos((Constants.FieldConstants.FieldX - mDrive.getPose().getX())
-          / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.RED_SPEAKER.getTranslation()));
+      tetha = Units.radiansToDegrees(
+          Math.acos((Constants.FieldConstants.FieldX - RobotX)
+              / mDrive.getPose().getTranslation().getDistance(Constants.FieldConstants.RED_SPEAKER.getTranslation())));
+      tAngle = -tetha;
     }
-    if (mDrive.getPose().getY() < Constants.FieldConstants.BLUE_SPEAKER.getY())
-      tAngle = tAngle * -1;
+
+    if (RobotY < Constants.FieldConstants.BLUE_SPEAKER.getY())
+      tAngle = -tAngle;
 
     return tAngle;
   }
